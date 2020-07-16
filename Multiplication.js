c3f00875304a6f86ef9a16584beeb7caadae2e78
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-15 20:58:28 -0500

    Added prompts for rows and columns

--- Multiplication.js
+++ Multiplication.js
@@ -2,6 +2,12 @@
 <head>
 
   <title>Multiplication Table</title>
-  
+  <script type="text/javascript">
+    
+    //Get the number of rows and columns from user
+    var rows = prompt("Enter the number of rows for the table?");
+    var cols = prompt("Enter the number of columns for the table?");
+    
+  </script>
 </head>
 </html>
