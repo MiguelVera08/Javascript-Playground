6d0c114e6b80ed8225451337fba9fac704022edb
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-16 22:02:43 -0500

    Added if statements to help with empty input value

--- Multiplication.js
+++ Multiplication.js
@@ -8,6 +8,12 @@
     var rows = prompt("Enter the number of rows for the table?");
     var cols = prompt("Enter the number of columns for the table?");
     
+  //If statement incase user does not enter value or leaves input empty
+  if(rows == "" || rows == null)
+    rows = 10;
+  if(cols == "" || cols == null)
+    cols = 10;
+    
   </script>
 </head>
 </html>
