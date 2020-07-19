684f30c3181d55cc48b168a909525dde2b5521dd
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-18 23:06:32 -0500

    Added input error divs

--- Forms.js
+++ Forms.js
@@ -1,1 +1,15 @@
-
+<html>
+  <title>Enter Information</title>
+  <script type="text/javascript">
+    
+    //Divs for input error messages
+    var divs = new Array();
+    divs[0] = "errFirst";
+    divs[1] = "errLast";
+    divs[2] = "errEmail";
+    divs[3] = "errUid";
+    divs[4] = "errPassword";
+    divs[5] = "errConfirm";
+    
+  </script>
+</html>
