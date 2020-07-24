0e161abb45175b0282736526ac1dc745dac749d6
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-23 21:00:32 -0500

    Update Forms.js

--- Forms.js
+++ Forms.js
@@ -57,6 +57,15 @@
       }
       //validation function
       function finalValidate(){
+        var count = 0;
+        for(i=0;i<6;i++){
+          var div = divs[i];
+          if(document.getElementById(div).innerHTML == "OK!"){
+              count = count + 1;
+            }
+          
+          }
+        }
       }
   </script>
 </html>
