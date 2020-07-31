084e78d60c09fa9c37f28b5f04aadcc7efaf43cd
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-30 21:40:33 -0500

    added css styling

--- Spherevolume.js
+++ Spherevolume.js
@@ -1,8 +1,12 @@
 //Javascript Program to Calculate the volume of a sphere
 
 <html lang="en">
-  <head>
-  <meta charset="utf-8">
-    <title>Find the Volume of a Sphere</title>
-  </head>
+<head>
+<meta charset="utf-8">
+<title>Find the Volume of a Sphere</title>
+<style>
+  body{padding-top: 30px;}
+  label,input{display: block;}
+</style>
+</head>
 </html>
