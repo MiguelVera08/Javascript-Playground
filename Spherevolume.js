41a1dc9dbd9639ca8f83415bf1195ab5973b1001
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-02 19:59:55 -0500

    Update Spherevolume.js

--- Spherevolume.js
+++ Spherevolume.js
@@ -23,6 +23,9 @@
         var radius = document.getElementById('radius').value;
         radius = Math.abs(radius);
         volume = (4/3) * Math.PI * Math.pow(radius< 3);
+        volume = volume.toFixed(4);
+        document.getElementById('volume').value = volume;
+        return false;
     }
   </script>
 </body>
