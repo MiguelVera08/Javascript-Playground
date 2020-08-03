faf8174e539c7c80fe042f4468fdd90e93908c1e
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-02 19:58:58 -0500

    update to volume sphere function

--- Spherevolume.js
+++ Spherevolume.js
@@ -21,6 +21,8 @@
       function volume_sphere(){
         var volume;
         var radius = document.getElementById('radius').value;
+        radius = Math.abs(radius);
+        volume = (4/3) * Math.PI * Math.pow(radius< 3);
     }
   </script>
 </body>
