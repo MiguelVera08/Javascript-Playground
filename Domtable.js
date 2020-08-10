5c245e4833b007967a3a245e24a12c3150cf2c26
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-09 21:05:17 -0500

    Update Domtable.js

--- Domtable.js
+++ Domtable.js
@@ -29,7 +29,7 @@ body {margin: 30px;}
           cn = window.prompt("Input the column number(0,1)","0");
           content = window.prompt("Input the Cell content");
           var x=document.getElementById('myTable').rows[parseInt(rn,10)].cells;
-          
+          x[parseInt(cn,10)].innerHTML=content;
          }
   </script>
 
