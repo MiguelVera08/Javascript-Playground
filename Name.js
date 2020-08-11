47dccd43f88f7979e337da6464068d243d8452af
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-10 21:24:01 -0500

    Update Name.js

--- Name.js
+++ Name.js
@@ -1,2 +1,8 @@
 //Javascript function that gets the values of First and Last name of a form
 
+<html>
+  <head>
+  <meta charset=utf-8 />
+  <title>Return Values of First and Last name of form.</title>
+  </head>
+<body>
