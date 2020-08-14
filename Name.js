9694da436819be0d361e1429dd2b3e82b4cb4399
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-13 21:18:12 -0500

    Update Name.js

--- Name.js
+++ Name.js
@@ -20,6 +20,7 @@
         var x = document.getElementById("form1");
         for (var i=0;i<x.length;i++){
           if (x.elements[i].value!='Submit'){
+            console.log(x.elements[i].value);
           }
         }
     }
