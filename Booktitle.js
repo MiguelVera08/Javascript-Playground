f383b2a1c5becac18371498806f2bacd2f565a37
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-14 21:40:53 -0500

    Update Booktitle.js

--- Booktitle.js
+++ Booktitle.js
@@ -1,1 +1,10 @@
 //Javascript program that displays the reading status of an array of objects
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Book Library</title>
+</head>
+<body>
+</body>
+</html>
