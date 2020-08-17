bc4d296cacbdc90b6cd58606e550862308c09edc
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-08-16 21:38:55 -0500

    Update Booktitle.js

--- Booktitle.js
+++ Booktitle.js
@@ -22,7 +22,18 @@
         title: 'Mockingjay: The Final Book of The Hunger Games',
         author: 'Suzanne Collins',
         readingStatus: false
-      }];  
+      }];
+
+      for (var i = 0; i < library.length; i++){
+        
+        var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+        
+        if(library[i].readingStatus){
+          console.log("Already read " + book);
+        }else{
+          
+        {
+      }
   </script>
 </body>
 </html>
